3d84611ba0ceb950cf8c74bcdc710cc1bbd3860e
shlish7 <sharonshlishman@gmail.com>
2024-09-18 21:01:44 +0300

    test

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -36,7 +36,7 @@ export function Profile() {
                         </section>
                     </section>
 
-
+                    <h1>Test</h1>
                 </header>
                 <div className="profile-main-container">
 
