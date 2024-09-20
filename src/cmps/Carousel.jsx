27d9b73389e067ab8b7ae4400d06f36bc1ef5228
shlish7 <sharonshlishman@gmail.com>
2024-09-20 17:01:20 +0300

    Carousel + home page

---
+++ src/cmps/Carousel.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import '../assets/styles/cmps/carousel.css'
+import Arrow from '../assets/svg/carousel-arrow.svg?react'
+import chocolateCake from '../assets/images/chocolate_cake.jpg'
+import Potatos from '../assets/images/potatos.jpg'
+import Petitim from '../assets/images/petitim.jpg'
+
+
+export function Carousel(props) {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+
+    const carouselImages = [
+        {
+            name: 'Chocolate Cake',
+            src: chocolateCake
+        },
+        {
+            name: 'Potatos',
+            src: Potatos
+        },
+        {
+            name: 'Petitim',
+            src: Petitim
+        }
+    ]
+
+    function onChangeImg(event) {
+        event.stopPropagation()
+        const { value } = event.currentTarget
+        const arrowDirection = value
+        console.log('target', event.currentTarget);
+        console.log('arrowDirection', arrowDirection);
+        if (arrowDirection === 'left') {
+            const newIndex = currentIndex === 0 ? carouselImages.length - 1 : currentIndex - 1;
+            setCurrentIndex(newIndex);
+        } else if (arrowDirection === 'right') {
+            const newIndex = currentIndex === carouselImages.length - 1 ? 0 : currentIndex + 1;
+            setCurrentIndex(newIndex);
+        }
+
+    }
+
+    return (<>
+
+        <div className="carousel-container">
+            <div className="carousel-arrow">
+                <button className='carousel-btn' value='left' onClick={onChangeImg}>
+                    <Arrow className='left-arrow-icon' />
+                </button>
+            </div>
+            <div className="carousel-main-frame">
+                <div className="img-container">
+                    <img
+                        src={carouselImages[currentIndex].src}
+                        alt={carouselImages[currentIndex].name} />
+                    {/* <div className="dot"></div> */}
+                    
+                    {carouselImages.length>1 &&
+                    <div className="dot-container">
+                        {carouselImages.map((_, idx) => (
+                            <span
+                                key={idx}
+                                className="dot"
+                                style={{ backgroundColor: currentIndex === idx ? 'white' : 'white' }}
+                            ></span>
+                        ))}
+                    </div> }
+                </div>
+            </div>
+            <div className="carousel-arrow">
+                <button className='carousel-btn' value='right' onClick={onChangeImg}>
+                    <Arrow className='right-arrow-icon' />
+                </button>
+            </div>
+
+
+        </div>
+    </>
+    )
+}
+
+
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,57 @@
 import { SideBar } from "../cmps/SideBar";
+import { Carousel } from "../cmps/Carousel";
+import ImageAvatars from '../cmps/ImageAvatars'
+import { Link } from 'react-router-dom'
+import Verified from '../assets/svg/verified.svg?react'
+import MoreOptions from '../assets/svg/more-options-icon.svg?react'
+import hazinor from '../assets/images/hazinor.jpg'
 
 export function HomePage() {
+
     return (
         <section className="instagram-home-page">
             <header className="home-header">header</header>
             <aside className="home-left-side-bar">
-                <SideBar/>
+                <SideBar />
             </aside>
-            <main className="home-feed">feed</main>
-            <aside className="home-right-suggestion">suggestion</aside>
-        </section >
+            <main className="home-feed">
+                <section className="home-feed-container">
+                    <section className="img-title-container">
+                        <section className="avatar">
+                            <ImageAvatars img={hazinor} avatarHeight='30px !important' avatarWidth='30px !important' />
+                        </section>
+                        <section className="home-page-img-title">
+                            <section className="home-user-title-container">
+                                <Link to="/" className="home-title-user-name">User.Name</Link>
+                                <Verified class='home-title-verified' />
+                                <span className='home-title-dot'>.</span>
+                                <time datetime="" className='home-posted-time'>15h</time>
+
+                            </section>
+                            <section className="home-title-details">
+                                <Link to="/" >
+                                    <span className="home-title-caption">veneris
+                                        <span>.</span>
+                                        Vivaldi Winter Drill #2
+                                    </span>
+
+                                </Link>
+
+                            </section>
+
+                        </section>
+
+                        <section className="img-title-more-option">
+                            <MoreOptions className='home-more-options-icon' />
+                        </section>
+                    </section>
+                    <Carousel></Carousel>
+                </section>
+
+            </main>
+
+            {/* <aside className="home-right-suggestion">suggestion</aside> */}
+        </section>
     )
 }
 
