dee90166113622d9117151057c91871828acf192
orpaztal <orpaztal1@gmail.com>
2024-09-28 15:43:09 +0300

    cleanup

--- src/store/user.actions.js
+++ src/store/user.actions.js
@@ -1,7 +1,6 @@
 import { userService } from '../services/user.service'
 import { socketService } from '../services/socket.service'
 import { store } from '../store/store'
-
 import { showErrorMsg } from '../services/event-bus.service'
 import { LOADING_DONE, LOADING_START } from './system.reducer'
 import { REMOVE_USER, SET_USER, SET_USERS, SET_WATCHED_USER } from './user.reducer'
