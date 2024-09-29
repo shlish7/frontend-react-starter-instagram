252a9485b0e58f67bc48efac5a6dca3c6e980b5e
shlish7 <sharonshlishman@gmail.com>
2024-09-29 13:57:09 +0300

    Added new comment to feed item

--- src/cmps/FeedItem/CommnetsView.jsx
+++ src/cmps/FeedItem/CommnetsView.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import EmojiPicker from 'emoji-picker-react';
 import EmojiPickerIcon from '../../assets/svg/emoji-picker.svg?react';
+import LikeCommentIcon from '../../assets/svg/like-comment-icon.svg?react';
 
 export function CommnetsView({ onOpenFeedItem, handleCommentSubmit }) {
 
   const [isEmojiPicker, setIsEmojiPicker] = useState(false)
   const [comment, setComment] = useState('')
+  const [newComment, setNewComment] =useState()
   const [postCommentBtn, setPostCommentBtn] = useState(false)
 
   // Event listener for closing emoji picker on 'Esc' key press
@@ -25,7 +27,7 @@ export function CommnetsView({ onOpenFeedItem, handleCommentSubmit }) {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, [isEmojiPicker]);
-  
+
 
   const onHandleCommentSubmit = () => {
     if (comment.trim() !== '') {
@@ -40,25 +42,33 @@ export function CommnetsView({ onOpenFeedItem, handleCommentSubmit }) {
   }
 
 
-function onEmojiClick(emojiData) {
-  console.log('Selected Emoji:', emojiData.emoji); // This should now print the selected emoji.
-  setComment(prevComment => prevComment + emojiData.emoji)// Append emoji to comment
-  setIsEmojiPicker(prev => !prev)
+  function onEmojiClick(emojiData) {
+    console.log('Selected Emoji:', emojiData.emoji); // This should now print the selected emoji.
+    setComment(prevComment => prevComment + emojiData.emoji)// Append emoji to comment
+    setIsEmojiPicker(prev => !prev)
 
-  if (comment.trim() !== '' || emojiData.emoji) {
-    setPostCommentBtn(true)
+    if (comment.trim() !== '' || emojiData.emoji) {
+      setPostCommentBtn(true)
+    }
   }
-}
 
-function onUpdateComment({target}){
-  const { value } = target
-  setComment(value)
-  if (value.trim() !== '') {
-    setPostCommentBtn(true)
-  } else {
+  function onUpdateComment({ target }) {
+    const { value } = target
+    setComment(value)
+    if (value.trim() !== '') {
+      setPostCommentBtn(true)
+    } else {
+      setPostCommentBtn(false)
+    }
+  }
+
+  function onDisplayNewComment(ev){
+    ev.stopPropagation()
+    setNewComment(prev => !prev)
+    setComment('')
     setPostCommentBtn(false)
+
   }
-}
 
 
   return (
@@ -68,30 +78,39 @@ function onUpdateComment({target}){
           <span onClick={onOpenFeedItem}>View all 61 comments</span>
         </Link>
       </section>
-      
+    {newComment &&
+      <section className="new-comment-container">
+        <section className='new-comment-details'>
+          <span className="new-comment-user-name">{'User Name'}</span>
+          <span className="new-comment">{'New Comment'}</span>
+        </section>
+        <LikeCommentIcon className='like-comment-icon' />
+      </section>
+}
+
       <section className="add-comment-and-emoji">
-      <textarea
-        type="text"
-        className="home-add-comment"
-        placeholder="Add a comment…"
- 
-        aria-label="Add a comment…"
-        value={comment} // Bind the state to the textarea's value
-        // onChange={(e) => setComment(e.target.value)} // Update the state on input change
-        onChange={onUpdateComment}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault();             
-            onHandleCommentSubmit(); 
-          }
-        }}
-      />
-      {postCommentBtn && <button className="post-comment-btn">Post</button>}
+        <textarea
+          type="text"
+          className="home-add-comment"
+          placeholder="Add a comment…"
+
+          aria-label="Add a comment…"
+          value={comment} // Bind the state to the textarea's value
+          // onChange={(e) => setComment(e.target.value)} // Update the state on input change
+          onChange={onUpdateComment}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' && !e.shiftKey) {
+              e.preventDefault();
+              onHandleCommentSubmit();
+            }
+          }}
+        />
+        {postCommentBtn && <button className="post-comment-btn" onClick={onDisplayNewComment}>Post</button>}
         <div className="emoji-picker-container">
           <EmojiPickerIcon className='emoji-picker' onClick={onOpenEmojiPicker} />
           {isEmojiPicker && (
             <div className="emoji-picker-wrapper">
-              <EmojiPicker onEmojiClick={onEmojiClick}/>
+              <EmojiPicker onEmojiClick={onEmojiClick} />
             </div>
           )}
         </div>
